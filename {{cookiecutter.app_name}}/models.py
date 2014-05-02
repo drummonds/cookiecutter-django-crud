704ae9024addb0e261a1ce64c605820240a1f1c8
DennisKvdM <dmeer@atlassian.co>
2014-05-02 14:44:58 -0700

    changed the models.py

--- {{cookiecutter.app_name}}/models.py
+++ {{cookiecutter.app_name}}/models.py
@@ -1,16 +1,17 @@
-from __future__ import unicode_literals
 from django.core.urlresolvers import reverse
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
 
-
-@python_2_unicode_compatible
 class {{ cookiecutter.model_name }}(models.Model):
     name = models.CharField(max_length=255)
 
     def __str__(self):
-        return '{{ cookiecutter.model_name }} ({})'.format(self.id or 'Unsaved')
+		return "%s" % (self.name)
+
+	def __repr__(self):
+		return self.__str__()
+
+	def __unicode__(self):
+		return u'%s' % (self.name)
 
     def get_absolute_url(self):
         return reverse('{{ cookiecutter.model_name|lower }}_detail', args=[str(self.id)])
-
